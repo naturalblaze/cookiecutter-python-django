7b719927280ec8f2a67840c86e0a538e249499bb
Ben Trachtenberg <btr1975@gmail.com>
2023-12-01 18:56:04 -0600

    added swagger ui params

--- {{cookiecutter.git_repo_name}}/{{cookiecutter.__app_name}}/web_app.py
+++ {{cookiecutter.git_repo_name}}/{{cookiecutter.__app_name}}/web_app.py
@@ -14,8 +14,15 @@ from {{cookiecutter.__app_name}}.routers import hello_world
 static_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')
 {% endif %}
 
+# Used to config swagger UI
+# Reference : https://swagger.io/docs/open-source-tools/swagger-ui/usage/configuration/
+swagger_ui_parameters = {
+    'docExpansion': 'none',
+    'filter': 'true'
+}
+
 web_app = FastAPI(title='{{ cookiecutter.git_repo_name }}', version=__version__,
-                  description='{{ cookiecutter.app_description }}')
+                  description='{{ cookiecutter.app_description }}', swagger_ui_parameters=swagger_ui_parameters)
 web_app.include_router(example.router)
 {% if cookiecutter.include_webpages == "y" %}
 web_app.include_router(hello_world.router)
