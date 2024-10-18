15b6c79bcda60a178f45fe015daa156289afbc6c
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2024-10-18 11:12:30 -0500

    added vuln checking to created, add all option to created, added min python version

--- {{cookiecutter.git_repo_name}}/make.bat
+++ {{cookiecutter.git_repo_name}}/make.bat
@@ -1,8 +1,17 @@
 @ECHO OFF
 REM Makefile for project needs
 REM Author: Ben Trachtenberg
-REM Version: 1.0.5
+REM Version: 1.0.6
 REM
+
+IF "%1" == "all" (
+    pytest --cov --cov-report=html -vvv
+    black {{cookiecutter.__app_name}}/
+    black tests/
+    pylint {{cookiecutter.__app_name}}\
+    pip-audit -r requirements.txt
+    GOTO END
+)
 
 IF "%1" == "build" (
     python -m build
@@ -31,6 +40,12 @@ IF "%1" == "dev-run" (
 
 IF "%1" == "format" (
     black {{cookiecutter.__app_name}}/
+    black tests/
+    GOTO END
+)
+
+IF "%1" == "check-vuln" (
+    pip-audit -r requirements.txt
     GOTO END
 )
 
@@ -44,12 +59,14 @@ IF "%1" == "gh-pages" (
 {% endif %}
 
 @ECHO make options
-@ECHO     build     To build a distribution
-@ECHO     coverage  To run coverage and display ASCII and output to htmlcov
-@ECHO     dev-run   To run the app
-@ECHO     format    To format the code with black
-@ECHO     pylint    To run pylint
-@ECHO     pytest    To run pytest with verbose option
+@ECHO     all                 To run coverage, format, pylint, and check-vuln
+@ECHO     build               To build a distribution
+@ECHO     check-vuln          To check for vulnerabilities
+@ECHO     coverage            To run coverage and display ASCII and output to htmlcov
+@ECHO     dev-run             To run the app
+@ECHO     format              To format the code with black
+@ECHO     pylint              To run pylint
+@ECHO     pytest              To run pytest with verbose option
 {% if cookiecutter.app_documents_location == 'github-pages' %}@ECHO     gh-pages  To create the GitHub pages{% endif %}
 
 :END
